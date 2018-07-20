4d096518eb8ffd624418450fe40d4814749c123b
apw601 <apw601@gmail.com>
2018-07-20 20:06:09 +0000

    automatically backed up by learn

---
+++ application_controllers.rb
@@ -0,0 +1,26 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+get '/' do
+    erb :index
+  end
+  
+  get '/index' do
+    erb :index
+  end
+  get '/happy' do
+    erb :happy
+  end
+  get '/upset' do 
+    erb :upset
+  end
+  get '/peaceful' do 
+    erb :peaceful
+  end
+  get '/stressed' do 
+    erb :stressed
+  end
+end
